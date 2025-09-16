95d78e9234602cae05716949fdb38f9f52837918
Elbialy <Elbialy0>
2025-09-16 17:49:38 +0300

    Add RabbitMQ messaging integration across services
    - Configured RabbitMQ in `user-service` and `email-service`.
    - Added `RabbitMQConfigurations` in both services for basic queue, exchange, and binding initialization.
    - Updated `docker-compose.yml` to include RabbitMQ service.
    - Updated application properties and dependencies for RabbitMQ in related services.
    - Adjusted `api-gateway` routes to align with updated endpoints.

---
+++ email-service /src/main/java/com/booky/email_service/config/RabbitMQConfigurations.java
@@ -0,0 +1,29 @@
+package com.booky.email_service.config;
+
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfigurations {
+    @Bean
+    public Queue userQueue(){
+        return new Queue("user.queue",true);
+    }
+    @Bean
+    public DirectExchange exampleExchange() {
+        return new DirectExchange("user.exchange");
+    }
+    @Bean
+    public Binding exampleBinding(Queue exampleQueue, DirectExchange exampleExchange) {
+        return BindingBuilder.bind(exampleQueue)
+                .to(exampleExchange)
+                .with("routing.key");
+    }
+
+
+}

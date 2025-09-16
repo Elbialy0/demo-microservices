50230b908640737db1b8da3f63a4be308d5fa215
Elbialy0 <mahmoudelbialy109@gmail.com>
2025-09-16 22:50:51 +0300

    Add JSON message converter and RabbitTemplate bean in RabbitMQ configuration

--- user-service/src/main/java/com/booky/user_service/config/RabbitMQConfigurations.java
+++ user-service/src/main/java/com/booky/user_service/config/RabbitMQConfigurations.java
@@ -1,10 +1,11 @@
 package com.booky.user_service.config;
 
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.DirectExchange;
-import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -28,6 +29,16 @@ public class RabbitMQConfigurations {
         return BindingBuilder.bind(exampleQueue)
                 .to(exampleExchange)
                 .with(ROUTING_KEY);
+    }
+    @Bean
+    public MessageConverter messageConverter(){
+        return new Jackson2JsonMessageConverter();
+    }
+    @Bean
+    public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory){
+        final var template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(messageConverter());
+        return template;
     }
 
 

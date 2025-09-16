ac844aba389edfbbb8879d69f90f62cf5770717b
Elbialy0 <mahmoudelbialy109@gmail.com>
2025-09-16 23:22:00 +0300

    Refactor RabbitMQ configuration for improved clarity and reusability
    - Updated queue and exchange names to `emailQueue` and `userExchange`.
    - Added constants for `EMAIL_QUEUE`, `EMAIL_EXCHANGE`, and `ROUTING_KEY`.
    - Introduced JSON message converter and RabbitTemplate bean for message handling.

--- email-service /src/main/java/com/booky/email_service/config/RabbitMQConfigurations.java
+++ email-service /src/main/java/com/booky/email_service/config/RabbitMQConfigurations.java
@@ -1,28 +1,44 @@
 package com.booky.email_service.config;
 
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.DirectExchange;
-import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfigurations {
+
+    public static final String EMAIL_QUEUE = "email-queue";
+    public static final String EMAIL_EXCHANGE = "user.exchange";
+    public static final String ROUTING_KEY = "routing.key";
+
     @Bean
-    public Queue userQueue(){
-        return new Queue("user.queue",true);
+    public Queue emailQueue(){
+        return new Queue(EMAIL_QUEUE,true);
     }
     @Bean
-    public DirectExchange exampleExchange() {
-        return new DirectExchange("user.exchange");
+    public DirectExchange userExchange() {
+        return new DirectExchange(EMAIL_EXCHANGE);
     }
     @Bean
-    public Binding exampleBinding(Queue exampleQueue, DirectExchange exampleExchange) {
+    public Binding userBinding(Queue exampleQueue, DirectExchange exampleExchange) {
         return BindingBuilder.bind(exampleQueue)
                 .to(exampleExchange)
-                .with("routing.key");
+                .with(ROUTING_KEY);
+    }
+    @Bean
+    public MessageConverter messageConverter(){
+        return new Jackson2JsonMessageConverter();
+    }
+    @Bean
+    public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory){
+        final var template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(messageConverter());
+        return template;
     }
 
 

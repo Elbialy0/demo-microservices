86f4d6d222d11f899d9d9dd12692b7a2eb8766f5
Elbialy0 <mahmoudelbialy109@gmail.com>
2025-09-16 23:22:22 +0300

    Implement `EmailServiceImpl` with initial structure

---
+++ email-service /src/main/java/com/booky/email_service/service/impl/EmailServiceImpl.java
@@ -0,0 +1,8 @@
+package com.booky.email_service.service.impl;
+
+import com.booky.email_service.service.EmailService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailServiceImpl implements EmailService {
+}

620e8742a6b31bd338c2c52f4404d78dbfea0484
Elbialy0 <mahmoudelbialy109@gmail.com>
2025-09-16 23:22:36 +0300

    Create `RabbitMQMessage` DTO with fields for message details, date, and email

---
+++ email-service /src/main/java/com/booky/email_service/dto/RabbitMQMessage.java
@@ -0,0 +1,19 @@
+package com.booky.email_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class RabbitMQMessage {
+    private String messageId;
+    private String message;
+    private Date messageDate;
+    private String email;
+}

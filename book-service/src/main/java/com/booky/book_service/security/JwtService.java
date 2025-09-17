71757ceefcaf33af18abe723c4410873f3efcff4
Elbialy0 <mahmoudelbialy109@gmail.com>
2025-09-17 23:58:38 +0300

    Add JWT validation logic and dependencies to `JwtService`
    - **JWT Validation**: Implement token validation method in `JwtService` to authenticate users and set roles from claims.
    - **Dependency Update**: Include `jjwt-api`, `jjwt-impl`, and `jjwt-jackson` dependencies in `pom.xml`.
    - **Configuration Update**: Add JWT secret key property in `application.yml`.

--- book-service/src/main/java/com/booky/book_service/security/JwtService.java
+++ book-service/src/main/java/com/booky/book_service/security/JwtService.java
@@ -1,7 +1,40 @@
 package com.booky.book_service.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.List;
 
 @Service
 public class JwtService {
+    @Value("${secret.key}")
+    private String SECRET;
+    public Authentication validate(String token) {
+        try {
+            Claims claims = Jwts.parser()
+                    .verifyWith(getSecretKey())
+                    .build()
+                    .parseSignedClaims(token)
+                    .getPayload();
+            String username = claims.getSubject();
+            List<String > roles = claims.get("roles", List.class);
+            var authorities = roles.stream().map(SimpleGrantedAuthority::new).toList();
+            return new UsernamePasswordAuthenticationToken(username,null,authorities);
+
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    private SecretKey getSecretKey(){
+        byte [] encodedKey = SECRET.getBytes();
+        return Keys.hmacShaKeyFor(encodedKey);
+    }
 }

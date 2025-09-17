4d5b5699b3783703d2a4ab4b8a18fbfe246bcf33
Elbialy0 <mahmoudelbialy109@gmail.com>
2025-09-17 11:23:06 +0300

    Enhance `MessageListener` to process incoming messages by invoking email preparation and sending services

--- email-service /src/main/java/com/booky/email_service/listener/MessageListener.java
+++ email-service /src/main/java/com/booky/email_service/listener/MessageListener.java
@@ -2,13 +2,27 @@ package com.booky.email_service.listener;
 
 import com.booky.email_service.config.RabbitMQConfigurations;
 import com.booky.email_service.dto.RabbitMQMessage;
+import com.booky.email_service.service.EmailPrep;
+import com.booky.email_service.service.EmailService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
+
 @Component
+@Slf4j
+@RequiredArgsConstructor
 public class MessageListener {
+    public static final String SUBJECT = "Verification Email";
+    public static final String VERIFICATION_TEMPLATE_HTML = "verification-template.html";
+    private final EmailService emailService;
+    private final EmailPrep emailPrep;
     @RabbitListener(queues = RabbitMQConfigurations.EMAIL_QUEUE)
     public void listen(RabbitMQMessage message){
-
+        log.info("Received message: {}", message);
+        emailService.sendEmail(emailPrep.prepareVerificationEmail(message.getMessage()),message.getEmail()
+        , SUBJECT, VERIFICATION_TEMPLATE_HTML);
     }
 }
